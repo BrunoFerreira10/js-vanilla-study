df058bb04b30ae0e235352f2ce5fe0d5f2ffcdbc
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2022-04-09 19:26:41 -0300

    Added responsive-navbar

---
+++ responsive-navbar/public/js/app.js
@@ -0,0 +1,10 @@
+const toggleButton = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+
+toggleButton.addEventListener('click', event => {
+  if(links.classList.contains('show-links')){
+    links.classList.remove('show-links')
+  } else {
+    links.classList.add('show-links')
+  }
+})
